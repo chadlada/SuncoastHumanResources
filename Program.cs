fd9dca7522eb82b8d1a1569b076b9247972152ea
Chad <chadlada@gmail.com>
2021-12-21 12:19:43 -0500

    Fixed errors so far

--- Program.cs
+++ Program.cs
@@ -53,15 +53,82 @@ namespace SuncoastHumanResources
         static void Main(string[] args)
         {
             var employees = new List<Employee>();
-            var employee = new Employee();
+
 
             DisplayGreeting();
 
-            employee.Name = PromptForString("What is your name? ");
-            employee.Department = PromptForInteger("What is your department number? ");
-            employee.Salary = PromptForInteger("What is your yearly salary (in dollars)? ");
+            // Should we keep showing the menu?
+            var keepGoing = true;
+            // While the user hasn't said QUIT yet
+            while (keepGoing)
+            {
+                // Insert a blank line then prompt them and get their answer (force uppercase)
+                Console.WriteLine();
+                Console.Write("What do you want to do?\n (A)dd an employee\n(F)ind an employee\n(S)how all the employees\n (Q)uit\n: ");
+                var choice = Console.ReadLine().ToUpper();
+                if (choice == "Q")
+                {
+                    // They said quit, so set our keepGoing to false
+                    keepGoing = false;
+                }
+                else
+                if (choice == "F")
+                {
+                    // -Create a var named ** foundEmployee ** 
+                    Employee foundEmployee = null;
+                    // - Prompt for the name 
+                    var nameToSearchFor = PromptForString("What name are you looking for?");
+                    // - Loop through the list to look for a match
+                    foreach (var employee in employees)
+                    {
+                        // - If we find one, update 'foundEmployee' 
+                        if (employee.Name == nameToSearchFor)
+                        {
+                            foundEmployee = employee;
+                        }
+                    }
+                    // - After the loop, 'foundEmployee' is either 'null' (not found) or refers to the matching item 
+                    if (foundEmployee == null)
+                    // - Show a message if 'null', 
+                    {
+                        Console.WriteLine("No such person!");
+                    }
+                    else
+                    {
+                        // otherwise show the details.
+                        Console.WriteLine($"{foundEmployee.Name} is in department {foundEmployee.Department} and makes ${foundEmployee.Salary}");
+                    }
+                }
+                else
+                if (choice == "S")
+                {
+                    // READ (Out of CRUD)
+                    foreach (var employee in employees)
+                    {
+                        Console.WriteLine($"{employee.Name} is in department {employee.Department} and makes ${employee.Salary}");
+                    }
+                }
+                else
+                {
+                    // CREATE (out of CRUD)
 
-            Console.WriteLine($"Hello, {employee.Name} you make {employee.MonthlySalary()} dollars per month.");
+
+
+
+                    var employee = new Employee();
+                    // Make a new employee object
+
+                    // Prompt for values and save them in the employee's properties
+                    employee.Name = PromptForString("What is your name? ");
+                    employee.Department = PromptForInteger("What is your department number? ");
+                    employee.Salary = PromptForInteger("What is your yearly salary (in dollars)? ");
+
+                    // Add it to the list
+                    employees.Add(employee);
+                }
+
+                // end of the `while` statement
+            }
         }
     }
 }
